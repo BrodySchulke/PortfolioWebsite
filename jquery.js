1741953518709e42b1da1a7ce30be186ed6e95be
BrodySchulke <brodyschulke@hotmail.com>
2016-12-07 10:03:05 -1200

    Title, Tabs, Fades, Stylings

--- jquery.js
+++ jquery.js
@@ -1,7 +1,43 @@
-$(document).ready(function(){
-  $(".ProjectInfo").hide();
-  $(".ProjectTitle").click(function(){
-    $(this).next().slideToggle();
-  });
+function displayContent(evt, tabName){
+  var i, tabContents, tabLinks;
+  tabContents = document.getElementsByClassName("tabContent");
+  for(i = 0; i < tabContents.length; i++) {
+    tabContents[i].style.display = "none";
+  }
+  tabLinks = document.getElementsByClassName("tabLink");
+  for(i = 0; i <tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+  $("#"+tabName).fadeIn();
+  evt.currentTarget.className += " active";
+}
 
+function openTab(evt, tabName){
+  var i, subTabs, tabContents, tabLinks;
+  subTabs = document.getElementsByClassName("subTabs");
+  for(i = 0; i < subTabs.length; i++) {
+    subTabs[i].style.display = "none";
+  }
+  tabContents = document.getElementsByClassName("tabContent");
+  for(i = 0; i < tabContents.length; i++) {
+    tabContents[i].style.display = "none";
+  }
+  tabLinks = document.getElementsByClassName("tabLink");
+  for(i = 0; i <tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
+  $("#"+tabName).fadeIn();
+  evt.currentTarget.className += " active";
+}
+
+$('.tabs').tabs({
+        select: function(event, ui) {
+            $(ui.panel).animate({opacity:0.1});
+        },
+        show: function(event, ui) {
+            $(ui.panel).animate({opacity:1.0},1000);
+        }
+});
+
+$(document).ready(function(){
 });
