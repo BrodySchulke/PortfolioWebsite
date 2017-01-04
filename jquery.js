11deb85d696ab4b2b37a3a19aa45eb5bc531586c
BrodySchulke <brodyschulke@hotmail.com>
2017-01-03 22:53:09 -0800

    Added Disassembler

--- jquery.js
+++ jquery.js
@@ -30,6 +30,29 @@ function openTab(evt, tabName){
   evt.currentTarget.className += " active";
 }
 
+function submitForm(evt) {
+  var firstName = $("#firstName").val();
+  var lastName = $("#lastName").val();
+  var senderEmail = $("#senderEmail").val();
+  var comments = $("#comments").val();
+  var captchaAnswer = $("#captchaAnswer").val();
+  var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+  if (!firstName || !lastName || !senderEmail || !comments || !captchaAnswer || captchaAnswer != 8 || !pattern.test(senderEmail)){
+    $("ValidSubmit").fadeOut(1);
+    $("#ErrorSubmit").fadeIn();
+    return;
+  }
+  else{
+    $("#ErrorSubmit").fadeOut(1);
+    $("#ValidSubmit").fadeIn();
+  }
+  $.ajax({
+    type: "POST",
+    data: $("#ContactForm").serialize(),
+    url: "contact.php"
+  });
+  $("#ContactForm").trigger('reset');
+}
 
 $(document).ready(function(){
   $("#startupTab").trigger("click");
@@ -41,9 +64,10 @@ $(document).ready(function(){
       $("#scrollUp").fadeOut("slow");
     }
   });
-
   $("#scrollUp").click(function(){
    // Animate the scrolling motion.
    $("html, body").animate({ scrollTop:0 },"slow");
   });
+
+
 });
